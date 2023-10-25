828078d1496254c8c296591a530b783c3cb74a07
Yonathan Cruz <yonathancruz2019@gmail.com>
2023-10-25 17:26:33 -0600

    Add ranking by tournamnet x stage

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,10 @@
 from  rest_framework import serializers
-from base.models import MatchDetail, Match, Team, Section, SectionName, Stage,StageName, Tournament
+from base.models import MatchDetail, MatchStrategy, Match, Team, Section, SectionName, Stage,StageName, Tournament
 
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields='__all__'
+        fields= ['team_id','team_name', 'team_acronym', 'team_slug']
 
 class StageNameSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,38 +15,45 @@ class SectionNameSerializer(serializers.ModelSerializer):
     class Meta:
         model = SectionName
         fields = ['section_name']
+
     
 class TournamentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tournament
-        fields = '__all__'
+        fields = ['tournament_id', 'tournament_name', 'tournament_slug', 'tournament_sport', 'tournament_start_date', 'tournament_end_date']
 
 class StageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stage
-        fields = '__all__'
+        fields = ['tournament_id', 'stage_name', 'stage_type', 'stage_slug']
     stage_name = StageNameSerializer(many=False)
     tournament_id = TournamentSerializer(many=False)
 
 class SectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Section
-        fields = '__all__'
+        fields = ['section_name', 'stage_id']
 
     section_name = SectionNameSerializer(many=False)
     stage_id = StageSerializer(many=False)
 
+class MatchStrategy(serializers.ModelSerializer):
+    class Meta:
+        model = MatchStrategy
+        fields = ['match_strategy_name']
+
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Match
-        fields = '__all__'
+        fields = ['match_type', 'match_state', 'match_mode', 'match_strategy', 'section_id']
 
     section_id = SectionSerializer(many=False)
-    
+    match_strategy = MatchStrategy(many=False)
+
 class MatchDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = MatchDetail
-        fields = '__all__'
+        fields = ['match_id','match_detail_win', 'team_id']
 
     match_id = MatchSerializer(many=False)
     team_id = TeamSerializer(many=False)
--- api/views.py
+++ api/views.py
@@ -1,48 +1,157 @@
-from base.models import MatchDetail 
+from base.models import MatchDetail, Team
 from .serializers import MatchDetailSerializer
-
+from django.core import serializers
+from django.db.models import (
+    Q,
+    F,
+    Subquery,
+    IntegerField,
+    ExpressionWrapper,
+    DateTimeField,
+)
 from rest_framework import status, filters, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
 
 class RankingStatsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = MatchDetail.objects.all();
-    serializer_class = MatchDetailSerializer;
-   
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter];
+    queryset = MatchDetail.objects.all()
+    serializer_class = MatchDetailSerializer
 
-    search_fields = ['idMatch__idSection__idStage__name'];
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
 
-    filterset_fields = {
-        'idMatch__idSection__idStage__name': ['exact']
-    };
-    ordering_fields = ['idMatch'];
+    search_fields = ["idMatch__idSection__idStage__name"]
+
+    filterset_fields = {"idMatch__idSection__idStage__name": ["exact"]}
+    ordering_fields = ["idMatch"]
+
 
 class RankingStatsView(APIView):
     def get(self, request, idTournament, format=None):
-        stage =  request.query_params.get('stage');
-        queryset = MatchDetail.objects.filter(
-            match_detail_match_team_integrant=True, 
-            match_id__section_id__stage_id__stage_name__stage_name_name__exact=stage,
-            match_id__section_id__stage_id__tournament_id__tournament_id__exact=idTournament);
-        
+        listTeam = []
+        listQuerysetMoreMatchDetail = []
+        groupPhases = ["Groups", "Play In Groups"]
+        stage = request.query_params.get("stage")
+        print(stage)
+        if not stage in groupPhases:
+            queryset = MatchDetail.objects.filter(
+                # match_detail_match_team_integrant=True,
+                match_id__section_id__stage_id__stage_name__stage_name_name__contains=stage,
+                match_id__section_id__stage_id__tournament_id__tournament_id__exact=idTournament,
+            )
+        else:
+            queryset = MatchDetail.objects.filter(
+                Q(
+                    match_id__section_id__stage_id__stage_name__stage_name_name__contains=groupPhases[
+                        0
+                    ]
+                )
+                | Q(
+                    match_id__section_id__stage_id__stage_name__stage_name_name__contains=groupPhases[
+                        1
+                    ]
+                ),
+                match_detail_match_team_integrant=True,
+                match_id__section_id__stage_id__tournament_id__tournament_id__exact=idTournament,
+            )
+
         if not queryset:
-            return Response({'message':'Your tournament id or stage are incorrect'}, status=status.HTTP_404_NOT_FOUND);
-        serializer = MatchDetailSerializer(queryset, many=True);
-        return Response(serializer.data, status=status.HTTP_200_OK);
+            return Response(
+                {"message": "Your tournament id or stage are incorrect"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = MatchDetailSerializer(queryset, many=True)
+        for data in serializer.data:
+            for key, value in data.items():
+                if key == "team_id":
+                    print(dict(value))
+                    data = dict(value)
+                    data = {
+                        "team_id": data["team_id"],
+                        "team_code": data["team_acronym"],
+                        "team_name": data["team_name"],
+                        "rank": 1,
+                    }
+                    listTeam.append(data)
+        avoidRepitedTeam = []
+        for team in listTeam:
+            if not team['team_id'] in avoidRepitedTeam:
+                six_months_ago = timedelta(
+                    days=30 * 6
+                )  # Assuming an average of 30 days per month
+
+                # Calculate the datetime for 6 months ago relative to the time of the query
+                six_months_ago_datetime = ExpressionWrapper(
+                    F(
+                        "match_id__section_id__stage_id__tournament_id__tournament_start_date"
+                    )
+                    - six_months_ago,
+                    output_field=DateTimeField(),
+                )
+
+                moreGameXTeam = MatchDetail.objects.filter(
+                    team_id__team_id__exact=team['team_id'],
+                    match_id__section_id__stage_id__tournament_id__tournament_start_date__gt=six_months_ago_datetime,
+                )
+
+
+                # Count win/loses
+                win = 0
+                lose = 0
+                for game in moreGameXTeam:
+                    if game.match_detail_win:
+                        win += 1
+                    else:
+                        lose += 1
+                listQuerysetMoreMatchDetail.append(
+                    {
+                        "team_id": team["team_id"],
+                        "team_code": team["team_code"],
+                        "team_name": team["team_name"],
+                        "win": win,
+                        "lose": lose,
+                        "played": win + lose,
+                    }
+                )
+                avoidRepitedTeam.append(team['team_id']);
+        
+        # Calculate rankings based on wins
+        rankings = sorted(
+            listQuerysetMoreMatchDetail,
+            key=lambda x: (x["win"], -x["played"]),
+            reverse=True,
+        )
+
+        # Add positions (ranks) to each item
+        rankings_with_positions = list(
+            map(
+                lambda x: {
+                    "team_id": x[1]["team_id"],
+                    "team_code": x[1]["team_code"],
+                    "team_name": x[1]["team_name"],
+                    "rank": x[0] + 1,
+                },
+                enumerate(rankings),
+            )
+        )
+        print("Ranking: ", rankings_with_positions)
+        return Response(rankings_with_positions, status=status.HTTP_200_OK)
+
 
 class RankingGlobalStatsView(APIView):
     def get(self, request):
-        numberOfTeams = request.query_params.get('number_of_teams') or 20;
+        numberOfTeams = request.query_params.get("number_of_teams") or 20
         queryset = []
-        serializer = MatchDetailSerializer(queryset, many=True);
-        return Response(serializer.data, status=status.HTTP_200_OK);
+        serializer = MatchDetailSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class RankingTeamStatsiView(APIView):
     def get(self, request):
-        numberOfTeams = request.query_params.get('team_rankings') or 20;
+        numberOfTeams = request.query_params.get("team_rankings") or 20
         queryset = []
-        serializer = MatchDetailSerializer(queryset, many=True);
-        return Response(serializer.data, status=status.HTTP_200_OK);
-
+        serializer = MatchDetailSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
